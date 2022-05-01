18845953ff612c3067b622d9ebb5c721459cd8b7
Biruk Erjamo <probiruk@gmail.com>
2022-05-01 16:22:56 +0300

    topic model done

---
+++ src/migrations/20220501125621-create-topic.js
@@ -0,0 +1,31 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("topics", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      parent_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable("topics");
+  },
+};
---
+++ src/models/topic.js
@@ -0,0 +1,31 @@
+const { Model } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  class topic extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate() {
+      // define association here
+    }
+  }
+  topic.init(
+    {
+      parent_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "topic",
+    }
+  );
+  return topic;
+};
